d0a04036c452303932649958202747cfabdc0b3b
KimNikita <ghj710@mail.ru>
2020-12-13 23:02:27 +0300

    Убрал clang

--- test/test_Polinom.cpp
+++ test/test_Polinom.cpp
@@ -23,23 +23,23 @@ TEST(TPolinom, can_multiply)
   string expP = "1543-1432-13451234";
   EXPECT_EQ(expP, res);
 }
-//
-//TEST(TPolinom, can_plus)
-//{
-//  TPolinom P, P1, P2;
-//  TMonom a(new double[3]{ 1,1,1 }, 3, -1);
-//  TMonom b(new double[3]{ 2,2,2 }, 3, 1);
-//  P1 += a;
-//  P1 += b;
-//  TMonom c(new double[3]{ 3,2,1 }, 3, 1);
-//  TMonom d(new double[3]{ 1,2,3 }, 3, -1);
-//  P2 += c;
-//  P2 += d;
-//  P = P1 + P2;
-//  string res = P.GetPolinom();
-//  string expP = "13211222-1123-1111";
-//  EXPECT_EQ(expP, res);
-//}
+
+TEST(TPolinom, can_plus)
+{
+  TPolinom P, P1, P2;
+  TMonom a(new double[3]{ 1,1,1 }, 3, -1);
+  TMonom b(new double[3]{ 2,2,2 }, 3, 1);
+  P1 += a;
+  P1 += b;
+  TMonom c(new double[3]{ 3,2,1 }, 3, 1);
+  TMonom d(new double[3]{ 1,2,3 }, 3, -1);
+  P2 += c;
+  P2 += d;
+  P = P1 + P2;
+  string res = P.GetPolinom();
+  string expP = "13211222-1123-1111";
+  EXPECT_EQ(expP, res);
+}
 
 TEST(TPolinom, can_sub)
 {
